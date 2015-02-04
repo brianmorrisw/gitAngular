a9145ca6599f20685283e241d8d2483e1e0647a4
fsalaysay <fsalaysay@gmail.com>
2015-02-04 17:25:22 +0800

    added working left sidebar with sample data from JSON

--- app/scripts/controllers/leftsidebar.js
+++ app/scripts/controllers/leftsidebar.js
@@ -9,14 +9,9 @@
  */
 angular.module('angularApp')
   .controller('LeftsidebarCtrl', function ($http, $scope) {
-  	$http.get('feeds/feed.json').
+  	$http.get('feeds/user-details/fred.json').
   		success(function (data) {
-  			$scope.users = data;
+  			$scope.owner = data;
   			console.log('success users');
-  		});
-  	$http.get('feeds/user-details.json').
-  		success(function (details) {
-  			$scope.owner = details;
-  			console.log('success user details');
   		});
   });
